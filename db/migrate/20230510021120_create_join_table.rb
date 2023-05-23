a509bf65e7600c67e3ef04ab71b81c526c13a10a
Luiz Romario Filho <luiz.lopes@ext.checkr.com>
2023-05-23 18:13:15 -0300

    Creating a join table

--- db/migrate/20230510021120_create_join_table.rb
+++ db/migrate/20230510021120_create_join_table.rb
@@ -1,8 +1,6 @@
 class CreateJoinTable < ActiveRecord::Migration[7.0]
   def change
-    create_join_table :tasks, :projects do |t|
-      # t.index [:task_id, :project_id]
-      # t.index [:project_id, :task_id]
+    create_join_table :tasks, :members do |t|
     end
   end
 end
